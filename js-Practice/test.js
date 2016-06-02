5c4d3ace32c80f5328c2f8cdfea20ae19b9522a6
datou <roughwin@qq.com>
2016-06-02 10:10:48 +0800

    6.2

--- js-Practice/test.js
+++ js-Practice/test.js
@@ -1,1 +1,9 @@
-document.write("hello")
+function fibonacci(n) {
+  var arr = new Array();
+  arr[0] = 0;
+  arr[1] = 1;
+  for (var i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
