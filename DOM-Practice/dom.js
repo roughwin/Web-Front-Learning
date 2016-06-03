60ede9ff503ef1cfbd0e25f59daae6dba84715ce
datou <roughwin@qq.com>
2016-06-03 14:58:26 +0800

    new

--- DOM-Practice/dom.js
+++ DOM-Practice/dom.js
@@ -1,55 +1,41 @@
-var year = document.getElementById("year");
-var month =  document.getElementById('month');
-var date = document.getElementById('date');
+var element = document.getElementById('rect');
+var example = document.getElementById('example');
 
-
-
-var isLeap;
-year.onchange = deal;
-month.onchange = deal;
-function deal(event) {
-    if(year.options[year.selectedIndex].value%4){
-        console.log(28);
+function getElementsByClassName(element, names){
+        var arr = [];
+        var nas = names.split(" ");
+        var els = element.children;
+        // var els = (element || document).getElementsByTagName('*');
+        for(var i=0; i<els.length; i++){
+            var k = els[i].className.split(" ");      //元素类名
+            var count = nas.length;  //用于记录是否匹配完成
         
-        isLeap=false;
-    }else{
-        console.log(29);
-        
-        isLeap=true;
-    }
-
-    if(!year.selectedIndex){
-        month.selectedIndex=0;
-        date.selectedIndex=0;
-    }
-
-
-    var nDays;
-    if ([1,3,5,7,8,10,12].indexOf(month.selectedIndex)>-1) {
-        nDays=31;
-    }else if(month.selectedIndex==2){        
-        if(isLeap){
-            nDays=29;            
-           
-        }else{
-            nDays=28;
-        }
-    }else{
-        nDays=30;
-    }
-    date.options.length=1;
-  
-    for (var i = 0; i < nDays; i++) {
-                option = document.createElement("option");
-                option.text = i+1;
-                option.value = i+1;
-                date.add(option,null);
+            //当要寻找的names长度小于或者等于元素类名长度时 names匹配完成即成功
+            if(nas.length <= els.length){
+                for(var j=0; j<k.length; j++){
+                    for(var n=0; n<nas.length; n++){
+                        if(k[j]==nas[n]){
+                                --count;
+                        }
+                    }
             }
-     if(year.selectedIndex==0){
-        month.selectedIndex=0;
-        date.options.length=1;
-    }
-    if (month.selectedIndex==0) {
-        date.options.length=1;
-    }
-}
+                if(count==0){
+                    arr.push(els[i]);
+                }
+            }
+            //当names长度大于元素类名长度时 匹配失败
+            else{
+                    break;
+            }
+        }
+        return arr;
+ }
+
+function fadeout(element){
+        var fadeout = setInterval(function(){
+            element.style.opacity -= .01; 
+            if(!element.style.opacity){
+                    clearInterval(fadeout);
+                }
+        },10);           //1s钟运行100次
+ }
